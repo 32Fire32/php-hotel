81b4d496fbb4193066440087283228c8eb3b7492
32Fire32 <nicola.gianfelici@hotmail.it>
2022-12-14 14:35:33 +0100

    import di bootstrap tramite cdn

--- index.php
+++ index.php
@@ -47,6 +47,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Php Hotel</title>
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
 </head>
 <body>
     <h1>I NOSTRI HOTEL</h1>
