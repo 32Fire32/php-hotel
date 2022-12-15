4dc45324ad7eaef9f9782c4f358b71b9f5e6c72e
32Fire32 <nicola.gianfelici@hotmail.it>
2022-12-15 10:43:06 +0100

    modifiche dopo correzione lezione

--- index.php
+++ index.php
@@ -38,54 +38,58 @@ $isVote = $_GET['vote'];
             'distance_to_center' => 50
         ],
     ];
+
+    $filtered_hotels = $hotels;
         if($isPark != 'choose'){
-            if($isPark == 'true')
-             {
-                $with_park = array_filter($hotels, function($data){
-                    return ($data['parking'] == true);
-                });
-                $hotels = $with_park;
+            $temp_hotels = array_filter($filtered_hotels, function($data){
+                return ($data['parking'] >= $_GET['parking']);
+            });
+            $filtered_hotels = $temp_hotels;
+            // if($isPark == 'true')
+            //  {
+            //     $with_park = array_filter($hotels, function($data){
+            //         return ($data['parking'] == true);
+            //     });
+            //     $hotels = $with_park;
                 
-            } else if($isPark == 'false'){
-                $without_park = array_filter($hotels, function($data){
-                    return ($data['parking'] == false);
-                });
-                $hotels = $without_park;
-            }
+            // } else if($isPark == 'false'){
+            //     $without_park = array_filter($hotels, function($data){
+            //         return ($data['parking'] == false);
+            //     });
+            //     $hotels = $without_park;
+            // }
         }
    
 
 if($isVote != 'choose'){
-    if($isVote == '1'){
-        $one_star_more = array_filter($hotels, function($data){
-            return ($data['vote'] >= 1);
+        $temp_hotels = array_filter($filtered_hotels, function($data){
+            return ($data['vote'] >= $_GET['vote']);
         });
-        $hotels = $one_star_more;
-    }
-    else if($isVote == '2'){
-        $two_star_more = array_filter($hotels, function($data){
-            return ($data['vote'] >= 2);
-        });
-        $hotels = $two_star_more;
-    }    
-    else if($isVote == '3'){
-        $three_star_more = array_filter($hotels, function($data){
-            return ($data['vote'] >= 3);
-        });
-        $hotels = $three_star_more;
-    }
-    else if($isVote == '4'){
-        $four_star_more = array_filter($hotels, function($data){
-            return ($data['vote'] >= 4);
-        });
-        $hotels = $four_star_more;
-    }  
-    else if($isVote == '5'){
-       $five_star_more = array_filter($hotels, function($data){
-           return ($data['vote'] >= 5);
-       });
-       $hotels = $five_star_more;
-   }
+        $filtered_hotels = $temp_hotels;
+//     else if($isVote == '2'){
+//         $two_star_more = array_filter($hotels, function($data){
+//             return ($data['vote'] >= 2);
+//         });
+//         $hotels = $two_star_more;
+//     }    
+//     else if($isVote == '3'){
+//         $three_star_more = array_filter($hotels, function($data){
+//             return ($data['vote'] >= 3);
+//         });
+//         $hotels = $three_star_more;
+//     }
+//     else if($isVote == '4'){
+//         $four_star_more = array_filter($hotels, function($data){
+//             return ($data['vote'] >= 4);
+//         });
+//         $hotels = $four_star_more;
+//     }  
+//     else if($isVote == '5'){
+//        $five_star_more = array_filter($hotels, function($data){
+//            return ($data['vote'] >= 5);
+//        });
+//        $hotels = $five_star_more;
+//    }
 }
 ?>
 
@@ -100,17 +104,18 @@ if($isVote != 'choose'){
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
 </head>
 <body>
+    <div class="container">
     <h1>I NOSTRI HOTEL</h1>
 <!-- FORM -->
-    <form action="index.php" method="GET">
+    <form action="index.php" method="GET" class="form-control">
         <label for="park">Parcheggio:</label>
-        <select name="parking" id="park">
+        <select class="form-select" name="parking" id="park">
             <option default value="choose">Scegli:</option>
             <option value="true">Con parcheggio</option>
             <option value="false">Senza parcheggio</option>
         </select>
         <label for="stars">Per voto:</label>
-        <select name="vote" id="stars">
+        <select class="form-select" name="vote" id="stars">
             <option default value="choose">Scegli:</option>
             <option value="1">1 Stella</option>
             <option value="2">2 Stelle</option>
@@ -118,13 +123,13 @@ if($isVote != 'choose'){
             <option value="4">4 Stelle</option>
             <option value="5">5 Stelle</option>
         </select>
-        <button type="submit">Invia</button>
+        <button class="btn btn-primary" type="submit">Invia</button>
     </form>
 <!-- TABELLA -->
-    <table class="table">
+    <table class="table table-striped">
     <thead><tr>
         <th scope="col">#</th>
-        <?php foreach ($hotels as $hotel) { ?>        
+        <?php foreach ($filtered_hotels as $hotel) { ?>        
             <th scope="col"><?php echo $hotel['name'] ?></th>
         <?php }  ?>
         </tr>
@@ -132,34 +137,31 @@ if($isVote != 'choose'){
     <tbody>
     <tr>
         <th scope="row">Descrizione</th>
-         <?php foreach ($hotels as $hotel) { ?>
-            <th scope="col"><?php echo $hotel['description'] ?></th>
+         <?php foreach ($filtered_hotels as $hotel) { ?>
+            <td scope="col"><?php echo $hotel['description'] ?></td>
         <?php }  ?>
     </tr>
     <tr>
         <th scope="row">Parcheggio</th>
-        <?php foreach ($hotels as $hotel) { ?>
-            <th scope="col"><?php 
-            if($hotel['parking'] == true){
-                echo 'Si';
-            } else {
-                echo 'No';
-            } ?></th>
+        <?php foreach ($filtered_hotels as $hotel) { ?>
+            <td scope="col"><?php echo $hotel['parking'] ? 'Si' : 'No'; ?> </td>
         <?php }  ?>
     </tr>
     <tr>
         <th scope="row">Voto</th>
-         <?php foreach ($hotels as $hotel) { ?>
-            <th scope="col"><?php echo $hotel['vote'] ?></th>
+         <?php foreach ($filtered_hotels as $hotel) { ?>
+            <td scope="col"><?php echo $hotel['vote'] ?></td>
         <?php }  ?>
     </tr>
     <tr>
         <th scope="row">Distanza dal centro</th>
-         <?php foreach ($hotels as $hotel) { ?>
-            <th scope="col"><?php echo $hotel['distance_to_center'] ?> km</th>
+         <?php foreach ($filtered_hotels as $hotel) { ?>
+            <td scope="col"><?php echo $hotel['distance_to_center'] ?> km</td>
         <?php }  ?>
     </tr>
     </tbody>
+    </div>
+    
 </table>
 
 </body>
